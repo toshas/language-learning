9620b0d8517c33e3c2ff7095918fd44b0f02a8aa
anton <anton.obukhov@gmail.com>
2024-02-16 22:32:44 +0100

    add cards grouping and filtering

--- language_cards.py
+++ language_cards.py
@@ -1,5 +1,5 @@
+import argparse
 import os
-import sys
 import re
 
 from PIL import Image
@@ -25,7 +25,7 @@ def get_prompt(ocr_text):
         "article and the plural form. "
         'Example format: "# das Buch = book, книга (die Bücher)". '
         'A verb should be converted to infinitive form in present, e.g., "ging" should become "gehen". '
-        'In the example sentence "Räumst du auf?", extract the separable prefix verb as "aufraumen". '
+        'In the example sentence "Räumst du auf?", extract the separable prefix verb as "aufraümen". '
         "Output each unique normalized verb with an English and Russian translation, and include the simple past and "
         "perfect participle forms. "
         'If used verb is transitive in the input text, please prepend "sich" in front of the verb in the output. '
@@ -93,7 +93,7 @@ def extract_cards_one_page(
 
 
 def parse_pdf_into_pages(
-    path_pdf_input,
+    pdf_path,
     path_output,
     first_page=None,
     last_page=None,
@@ -101,7 +101,7 @@ def parse_pdf_into_pages(
     thread_count=8,
 ):
     return convert_from_path(
-        path_pdf_input,
+        pdf_path,
         first_page=first_page,
         last_page=last_page,
         output_folder=path_output,
@@ -114,7 +114,7 @@ def parse_pdf_into_pages(
 
 
 def process_pdf_raw(
-    path_pdf_input,
+    pdf_path,
     path_output,
     first_page=None,
     last_page=None,
@@ -125,7 +125,7 @@ def process_pdf_raw(
     if not os.path.exists(marker_complete):
         os.makedirs(path_output_pages_raw, exist_ok=True)
         list_page_files = parse_pdf_into_pages(
-            path_pdf_input,
+            pdf_path,
             path_output_pages_raw,
             first_page=first_page,
             last_page=last_page,
@@ -194,7 +194,7 @@ def filter_responses(data, num_inferences=3, verbose=False):
                 continue
             if entry not in unique_lines:
                 unique_lines[entry] = line
-            if len(unique_lines[entry]) < len(line):
+            if len(unique_lines[entry]) < len(line):  # choose a longer flip side
                 unique_lines[entry] = line
         for k, v in unique_lines.items():
             if k not in out:
@@ -304,23 +304,132 @@ def extract_cards_all_pages(path_output, num_inferences=3, verbose=False):
     return path_output_cards
 
 
-if __name__ == "__main__":
-    assert len(sys.argv) >= 2, "Expecting at least a path to the PDF file"
-    path_pdf_input = sys.argv[1]
-    if len(sys.argv) >= 3:
-        path_output = sys.argv[2]
-    else:
-        path_output = os.path.splitext(path_pdf_input)[0] + "_output"
+def group_chapters(path_output, chapter_pages, kill_list, strip_list):
+    print("Grouping cards by chapters...")
 
-    verbose = False
-    longest_side = 1024
+    path_input_cards = os.path.join(path_output, "cards_pages_independent")
+    if chapter_pages is None:
+        path_output_by_chapter = os.path.join(path_output, "cards_final")
+        path_output_cumulative = None
+    else:
+        path_output_by_chapter = os.path.join(path_output, "cards_final_by_chapter")
+        path_output_cumulative = os.path.join(path_output, "cards_final_cumulative")
+    marker_complete = os.path.join(path_output_by_chapter, ".marker.complete")
+
+    chapter_cards = {}
+    cumulative_cards = {}
+    chapter_id = 0
+
+    def dump_chapter():
+        nonlocal chapter_cards, cumulative_cards, chapter_id
+        chapter_file = f"chapter{chapter_id:02d}.txt"
+
+        if path_output_cumulative is not None:
+            for k in list(chapter_cards.keys()):
+                if k not in cumulative_cards:
+                    cumulative_cards[k] = chapter_cards[k]
+                else:
+                    del chapter_cards[k]
+
+            cumulative_cards = dict(sorted(cumulative_cards.items()))
+            with open(os.path.join(path_output_cumulative, chapter_file), "w") as fp:
+                fp.write("\n".join([f"{k} = {v}" for k, v in cumulative_cards.items()]))
+
+        chapter_cards = dict(sorted(chapter_cards.items()))
+        with open(os.path.join(path_output_by_chapter, chapter_file), "w") as fp:
+            fp.writelines("\n".join([f"{k} = {v}" for k, v in chapter_cards.items()]))
+
+        chapter_cards = {}
+        chapter_id += 1
+
+    if not os.path.exists(marker_complete):
+        os.makedirs(path_output_by_chapter, exist_ok=True)
+        if path_output_cumulative is not None:
+            os.makedirs(path_output_cumulative, exist_ok=True)
+        pbar = tqdm(sorted(os.listdir(path_input_cards)))
+
+        for page_file in pbar:
+            if not page_file.startswith("page") or not page_file.endswith(".txt"):
+                continue
+            page_num = int(page_file[4:-4])
+            path_in = os.path.join(path_input_cards, page_file)
+
+            with open(path_in, "r") as fp:
+                page_cards = fp.read()
+            if strip_list is not None:
+                for s in strip_list:
+                    page_cards = page_cards.replace(s, "")
+            page_cards = page_cards.split("\n")
+            page_cards = [line.split("=") for line in page_cards]
+            assert all([len(splits) == 2 for splits in page_cards])
+            page_cards = {splits[0].strip(): splits[1].strip() for splits in page_cards}
+            if kill_list is not None:
+                page_cards = {k: v for k, v in page_cards.items() if k not in kill_list}
+            for k, v in page_cards.items():
+                if k not in chapter_cards or len(chapter_cards[k]) < len(v):
+                    chapter_cards[k] = v
+
+            if chapter_pages is not None and page_num in chapter_pages:
+                dump_chapter()
+
+        if len(chapter_cards) > 0:
+            dump_chapter()
+
+        with open(marker_complete, "w") as _:
+            pass
+
+    out = [path_output_by_chapter]
+    if chapter_pages is not None:
+        out.append(path_output_cumulative)
+    return out
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Extract language cards from a PDF")
+    parser.add_argument("pdf_path", type=str, help="PDF input")
+    parser.add_argument(
+        "--pdf_page_first", type=int, default=None, help="First page to process"
+    )
+    parser.add_argument(
+        "--pdf_page_last", type=int, default=None, help="Last page to process"
+    )
+    parser.add_argument(
+        "--out_path", type=str, default=None, help="Output directory path"
+    )
+    parser.add_argument(
+        "--resolution", type=int, default=1024, help="Optical recognition resolution"
+    )
+    parser.add_argument(
+        "--chapter_pages",
+        type=int,
+        nargs="+",
+        default=None,
+        help="List of page numbers with new chapters",
+    )
+    parser.add_argument("--kill", type=str, nargs="+", default=None, help="Cards keys to remove from the output")
+    parser.add_argument("--strip", type=str, nargs="+", default=None, help="Texts to strip from cards")
+    parser.add_argument(
+        "--verbose", action="store_true", help="Output more details about the process"
+    )
+    args = parser.parse_args()
+
+    if args.out_path is None:
+        args.out_path = os.path.splitext(args.pdf_path)[0] + "_output"
 
     openai_api_key = get_openai_api_key()
 
-    path_output_pages_raw = process_pdf_raw(
-        path_pdf_input, path_output, first_page=None, last_page=None
+    out_path_pages_raw = process_pdf_raw(
+        args.pdf_path,
+        args.out_path,
+        first_page=args.pdf_page_first,
+        last_page=args.pdf_page_last,
     )
-    path_output_pages_resized = resize_pages(path_output, longest_side=longest_side)
-    path_output_ocr = ocr_all_pages(path_output)
-    path_output_cards = extract_cards_all_pages(path_output, verbose=verbose)
+    out_path_pages_resized = resize_pages(args.out_path, longest_side=args.resolution)
+    out_path_ocr = ocr_all_pages(args.out_path)
+    out_path_cards_all_pages = extract_cards_all_pages(
+        args.out_path, verbose=args.verbose
+    )
+
+    out_paths_cards_chapters = group_chapters(args.out_path, args.chapter_pages, args.kill, args.strip)
+
     print("Done.")
